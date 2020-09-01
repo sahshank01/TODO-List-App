f35041d0ed0da0db3e0262f385cd1762f57e2027
shashank <shashanksah143@gmail.com>
2020-09-01 21:21:27 +0530

    added multiple events for better UX and changed UI of the app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,19 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
  $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
   $(this).parent().fadeOut(500,function(){
     $(this).remove();
   })
   event.stopPropagation();
 })
+
+$("input[type='text']").keypress(function(event){
+  if(event.which===13){
+    let todoText= $(this).val();
+  $(this).val("");
+  $("ul").append("<li><span>X </span>" + todoText + "</li>");
+  }
+});
